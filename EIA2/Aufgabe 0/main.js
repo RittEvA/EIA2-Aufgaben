b3f185b66901a7babda5dadb78e8d42de0821bf6
RittEvA <eva@familie-ritt.de>
2019-03-24 11:36:15 +0100

    wip

--- EIA2/Aufgabe 0/main.js
+++ EIA2/Aufgabe 0/main.js
@@ -1,1 +1,15 @@
+function YourName() {
+    document.getElementById("Text").innerHTML = "";
+    var Name = prompt("Nanu, wer bist den Du?");
+    if (Name == null) {
+        console.log("Sie haben den Vorgang abgebrochen");
+    }
+    else if (Name == "") {
+        alert("Wie heißen Sie denn?");
+    }
+    else {
+        console.log("Halli Hallo," + Name + "! Wie schön das Sie mich besuchen.");
+        document.getElementById("Text").innerHTML = "Halli Hallo, <span>" + Name + "</span>! Wie schön das Sie mich besuchen.";
+    }
+}
 //# sourceMappingURL=main.js.map
--- EIA2/Aufgabe 0/main.ts
+++ EIA2/Aufgabe 0/main.ts
@@ -1,7 +1,7 @@
 function YourName(){
     document.getElementById("Text").innerHTML="";
-    var Name = prompt("dein Name");
-    if (Name==0){
+    var Name = prompt("Nanu, wer bist den Du?");
+    if (Name==null){
         console.log("Sie haben den Vorgang abgebrochen");
     }
     else if(Name==""){
@@ -9,6 +9,6 @@ function YourName(){
     }
     else{
         console.log("Halli Hallo," + Name +"! Wie schön das Sie mich besuchen.");
-        document.getElementById("Text").innerHTML="Hallöchen<div class='YourName'>"+Name+"</div>. Danke für Ihren"
+        document.getElementById("Text").innerHTML="Halli Hallo, <span>"+Name+"</span>! Wie schön das Sie mich besuchen."
     }
 }
